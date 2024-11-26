4e5b8310a684d88745c61580e7d3003b8ea824be
fabienhinault <fabienhinault@users.noreply.github.com>
2024-11-26 16:42:09 +0100

    Update hexModelTest.js

--- test/hexModelTest.js
+++ test/hexModelTest.js
@@ -1,5 +1,5 @@
 import { strict as assert } from 'assert';
-import {Game, white, black} from '../hexModel.js';
+import {Game, white, black} from '../src/hexModel.js';
 
 
 describe('Hex', function () {
@@ -16,32 +16,65 @@ describe('Hex', function () {
       assert.equal(game.board[0][0].iCol, 0);
       assert.equal(game.board[0][0].getColumnLetter(), 'A');
     });
+
     it('should play on a Game constructed with 1', function () {
       const game = new Game(1);
       const isWinning = game.play(0, 0);
       assert(isWinning);
-      assert.equal(game.chains.get(white).length, 1);
-      assert(game.chains.get(white)[0].isWinning());
+      assert.equal(game.chains.get(white).size, 1);
+      assert(game.chains.get(white).values().next().value.isWinning());
+    });
+
+    it('should throw an error when playing on a colored hex', function () {
+      const game = new Game(1);
+      game.play(0, 0);
+      assert.throws(() => game.play(0, 0), new Error("illegal move"));
+    });
+
+    it('should throw an error when playing outside the board', function () {
+      const game = new Game(1);
+      assert.throws(() => game.play(-1, 0), new Error("out of board -1 0"));
     });
 
     it('should play on a Game constructed with 2', function () {
       const game = new Game(2);
       let isWinning = game.play(0, 0);
       assert(!isWinning);
-      assert.equal(game.chains.get(white).length, 1);
-      assert(!game.chains.get(white)[0].isWinning());
+      assert.equal(game.chains.get(white).size, 1);
+      assert(!game.chains.get(white).values().next().value.isWinning());
+
+      isWinning = game.play(1, 1);
+      assert(!isWinning);
+      assert.equal(game.chains.get(black).size, 1);
+      assert(!game.chains.get(black).values().next().value.isWinning());
+
+      isWinning = game.play(1, 0);
+      assert(isWinning);
+      assert.equal(game.chains.get(white).size, 1);
+      assert(game.chains.get(white).values().next().value.isWinning());
+    });
+
+    it('should play on a Game constructed with 2', function () {
+      const game = new Game(2);
+      let isWinning = game.play(0, 0);
+      assert(!isWinning);
+      assert.equal(game.chains.get(white).size, 1);
+      assert(!game.chains.get(white).values().next().value.isWinning());
+
+      isWinning = game.play(1, 1);
+      assert(!isWinning);
+      assert.equal(game.chains.get(black).size, 1);
+      assert(!game.chains.get(black).values().next().value.isWinning());
 
       isWinning = game.play(0, 1);
       assert(!isWinning);
-      assert.equal(game.chains.get(black).length, 1);
-      assert(!game.chains.get(black)[0].isWinning());
+      assert.equal(game.chains.get(white).size, 1);
+      assert(!game.chains.get(white).values().next().value.isWinning());
 
-      const neighbours = game.getNeighbourHexes(1, 0);
-      console.log(neighbours);
       isWinning = game.play(1, 0);
-      assert(!isWinning);
-      assert.equal(game.chains.get(white).length, 1);
-      assert(!game.chains.get(white)[0].isWinning());
+      assert(isWinning);
+      assert.equal(game.chains.get(black).size, 1);
+      assert(game.chains.get(black).values().next().value.isWinning());
     });
   });
 
