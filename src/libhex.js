4cf169c7fc3929bdf672d57012d394a73563212d
fabienhinault <fabienhinault@users.noreply.github.com>
2024-11-28 13:28:35 +0100

    Update libhex.js

--- src/libhex.js
+++ src/libhex.js
@@ -3,3 +3,7 @@
 export function range(size, startAt = 0) {
     return [...Array(size).keys()].map(i => i + startAt);
 }
+
+export function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
