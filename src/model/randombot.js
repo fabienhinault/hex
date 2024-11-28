ee2edef76e069654d39922598d8473dc72a34e69
fabienhinault <fabienhinault@users.noreply.github.com>
2024-11-28 13:25:03 +0100

    Create randombot.js

---
+++ src/model/randombot.js
@@ -0,0 +1,29 @@
+'use strict';
+
+import {Game} from './hexModel.js';
+import { getRandomInt } from '../libhex.js';
+
+export class RandomBot {
+    constructor(game) {
+        this.game = game;
+    }
+
+    play() {
+        console.log(this.game.toAscii());
+        let iRow = getRandomInt(0, this.game.size);
+        let iCol = getRandomInt(0, this.game.size);
+        while (true) {
+            while (iCol < this.game.size) {
+                const free = (this.game.board[iRow][iCol].color === undefined);
+                if (free) {
+                    const winningChain = this.game.play(iRow, iCol);
+                    return winningChain;
+                } else {
+                    iCol +=1
+                }
+            }
+            iRow = (iRow + 1) % this.game.size;
+            iCol = 0;
+        }
+    }
+}
