156a87d52a1bfd190230e5ce15a18215e566707c
fabienhinault <fabienhinault@users.noreply.github.com>
2025-01-20 12:29:35 +0100

    Update minMaxBot.js

--- src/model/minMaxBot.js
+++ src/model/minMaxBot.js
@@ -1,6 +1,5 @@
 'use strict';
 
-import { randArgMin } from '../libhex.js';
 import { Evaluator, MemorySequenceValueStorage } from './evaluator.js';
 import {black} from './hexModel.js';
 
@@ -11,17 +10,10 @@ export class MinMaxBot {
         this.evaluator = new Evaluator(game, new MemorySequenceValueStorage(game.size), black);
     }
 
-    getMoveValue(hex) {
-        return hex.game.after(hex.iRow, hex.iCol).toPositionString();
-        g.play(hex.iRow, hex.iCol);
-        return g.getRawValue();
-    }
-
     play() {
         console.log(this.game.getPrompt());
         if (this.game.getPossibleNexts().length > 0) {
-            this.evaluator.evaluateNextsSync(this.game.clock.getTime() + 900);
-            console.log(this.evaluator.sequenceValueStorage.values.size);
+            this.evaluator.evaluateNextsSync(this.evaluator, this.game.clock.getTime() + 1800);
         }
         const hexMin = this.evaluator.chooseNext();
         return this.game.play(hexMin.iRow, hexMin.iCol);
